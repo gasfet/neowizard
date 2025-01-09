df09b3b5a92838a684ce007454d32273ca575681
GASFET <49603992+gasfet@users.noreply.github.com>
2025-01-09 23:02:08 +0900

    28장 study코드 추가

---
+++ PyTorch/LEC28.py
@@ -0,0 +1,186 @@
+﻿import torch
+import numpy as np
+import pandas as pd
+from torch import nn
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import TensorDataset, DataLoader
+
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+print(f"using PyTorch version: {torch.__version__}, Device: {DEVICE}")
+
+FEATURE_NUMS = 4        # 입력층으로 들어가는 데이터 개수 feature
+SEQ_LENGTH = 5          # 정답을 만들기 위해 필요한 시점 개수 time step
+HIDDEN_SIZE = 4         # RNN 계열 계층을 구성하는 hidden state 개수
+NUM_LAYERS = 1          # RNN 계열 계층이 몇겹으로 쌓였는지 나타냄
+LEARNING_RATE = 1e-3    # 학습율
+BATCH_SIZE = 20         # 학습을 위한 배치사이즈 개수
+
+import FinanceDataReader as fdr
+
+df = fdr.DataReader('005930', '2020-01-01', '2024-06-30')
+
+df = df[ ['Open', 'High', 'Low', 'Volume', 'Close'] ]
+
+df.head(10)
+
+df['Close'].plot().grid()
+
+# train : test - 70 : 30 분리
+
+SPLIT = int(0.7*len(df))  # train : test = 7 : 3
+
+train_df = df[ :SPLIT ]
+test_df = df[ SPLIT: ]
+
+scaler_x = MinMaxScaler()  # feature scaling
+
+train_df.iloc[ : , :-1 ] = scaler_x.fit_transform(train_df.iloc[ : , :-1 ])
+test_df.iloc[ : , :-1 ] = scaler_x.fit_transform(test_df.iloc[ : , :-1 ])
+
+scaler_y = MinMaxScaler()  # label scaling
+
+train_df.iloc[ : , -1 ] = scaler_y.fit_transform(train_df.iloc[ : , [-1] ])
+test_df.iloc[ : , -1 ] = scaler_y.fit_transform(test_df.iloc[ : , [-1] ])
+
+def MakeSeqNumpyData(data, seq_length):
+
+    x_seq_list = []
+    y_seq_list = []
+
+    for i in range(len(data) - seq_length):
+        x_seq_list.append(data[ i:i+seq_length, :-1 ])
+        y_seq_list.append(data[ i+seq_length, [-1] ])
+
+    x_seq_numpy = np.array(x_seq_list)
+    y_seq_numpy = np.array(y_seq_list)
+
+    return x_seq_numpy, y_seq_numpy
+
+
+x_train_data, y_train_data = MakeSeqNumpyData(np.array(train_df), SEQ_LENGTH)
+
+x_test_data, y_test_data = MakeSeqNumpyData(np.array(test_df), SEQ_LENGTH)
+
+print(x_train_data.shape, y_train_data.shape)
+print(x_test_data.shape, y_test_data.shape)
+
+x_train_tensor = torch.FloatTensor(x_train_data).to(DEVICE)
+y_train_tensor = torch.FloatTensor(y_train_data).to(DEVICE)
+
+x_test_tensor = torch.FloatTensor(x_test_data).to(DEVICE)
+y_test_tensor = torch.FloatTensor(y_test_data).to(DEVICE)
+
+train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
+test_dataset = TensorDataset(x_test_tensor, y_test_tensor)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+
+class MyGRUModel(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, data):
+        h0 = torch.zeros(self.num_layers, data.size(0), self.hidden_size).to(DEVICE)
+
+        outputs, _ = self.gru(data, h0)
+        last_hs = outputs[:, -1, :]
+        prediction = self.fc(last_hs)
+
+        return prediction
+
+model = MyGRUModel(FEATURE_NUMS, HIDDEN_SIZE, NUM_LAYERS).to(DEVICE)
+
+loss_function = nn.MSELoss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+def model_train(dataloader, model, loss_function, optimizer):
+
+    model.train()
+
+    train_loss_sum = 0
+
+    total_train_batch = len(dataloader)
+
+    for inputs, labels in dataloader:
+
+        x_train = inputs.to(DEVICE)
+        y_train = labels.to(DEVICE)
+
+        outputs = model(x_train)
+        loss = loss_function(outputs, y_train)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_loss_sum += loss.item()
+
+    train_avg_loss = train_loss_sum / total_train_batch
+
+    return train_avg_loss
+
+def model_evaluate(dataloader, model, loss_function, optimizer):
+
+    model.eval()
+
+    with torch.no_grad():
+
+        val_loss_sum = 0
+
+        total_val_batch = len(dataloader)
+
+        for inputs, labels in dataloader:
+
+            x_val = inputs.to(DEVICE)
+            y_val = labels.to(DEVICE)
+
+            outputs = model(x_val)
+            loss = loss_function(outputs, y_val)
+
+            val_loss_sum += loss.item()
+
+        val_avg_loss = val_loss_sum / total_val_batch
+
+    return val_avg_loss
+
+from datetime import datetime
+
+train_loss_list = []
+
+start_time = datetime.now()
+
+EPOCHS = 200
+
+for epoch in range(EPOCHS):
+
+    avg_loss = model_train(train_loader, model, loss_function, optimizer)
+
+    train_loss_list.append(avg_loss)
+
+    if (epoch % 10 == 0):
+        print('epoch: ', epoch, ', train loss = ', avg_loss)
+
+end_time = datetime.now()
+
+print('elapsed time => ', end_time-start_time)
+
+
+import matplotlib.pyplot as plt
+
+plt.title('Loss Trend')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.grid()
+plt.plot(train_loss_list, label='train loss')
+plt.legend()
+plt.show()
